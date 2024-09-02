a20ea968d6d9e7e929f625562ab667de8c0b2ffb
KaySeaN11 <seann1005@naver.com>
2024-09-02 15:55:33 +0900

    ksh

--- ksh.py
+++ ksh.py
@@ -1,3 +1,9 @@
-print('helloworld')
+import pygame
 
-print('hi')
+pygame.init()
+
+screen_width = 480
+screen_height = 640
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("ksn")
