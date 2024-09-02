63426566dde717d1839e623bbb95757b03eff82b
KaySeaN11 <seann1005@naver.com>
2024-09-02 16:01:09 +0900

    ksh

--- ksh.py
+++ ksh.py
@@ -7,3 +7,11 @@ screen_height = 640
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("ksn")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
