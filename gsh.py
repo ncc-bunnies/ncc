11c1c0593432a239f7d133e57f00783691f35fc9
rlftkdgus11 <rlftkdgus11@gmail.com>
2024-09-02 15:55:15 +0900

    gsh

---
+++ gsh.py
@@ -0,0 +1,12 @@
+#길상현 코드
+import pygame
+
+pygame.init() #초기화
+
+#화면크기
+screen_width = 480 # 가로 크기
+screen_height = 640 
+pygame.display.set_mode((screen_width,screen_height))
+
+
+pygame.display.set_caption("스톤월드")
