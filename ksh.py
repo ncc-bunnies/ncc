6ed4c4daaf311d4850cdd3a31230fff618c0167f
KaySeaN11 <seann1005@naver.com>
2024-09-02 16:21:31 +0900

    ksh

--- ksh.py
+++ ksh.py
@@ -7,11 +7,14 @@ screen_height = 640
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("ksn")
+background = pygame.image.load("C:\\Users\\ncc8\\Desktop\\ncc\\ncc-1\\background.png")
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.blit(background, (0,0))
+    pygame.display.update()
 
 pygame.quit()
