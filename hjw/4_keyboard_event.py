beb3288e24589e980ac489e734ab7e8f026d35bc
bunnies722-do <neummasource@gmail.com>
2024-09-03 13:26:54 +0900

    hjw

--- hjw/4_keyboard_event.py
+++ hjw/4_keyboard_event.py
@@ -9,10 +9,10 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("정우")
 
 
-background = pygame.image.load("hjw\\asset\\background.png")
+background = pygame.image.load(r"ncc\hjw\asset\background.png")
+#섹
 
-
-character = pygame.image.load("hjw\\asset\\character.png")
+character = pygame.image.load(r"ncc\hjw\asset\character.png")
 character_size = character.get_rect().size # 이미지의 크기를 구해옴
 character_width = character_size[0]
 character_height = character_size[1]
@@ -39,14 +39,24 @@ while running:
                 to_y += 5
 
         if event.type == pygame.KEYUP:
-            if event == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 to_x = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 to_y = 0
 
     character_x_pos += to_x
     character_y_pos += to_y
 
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
     screen.blit(background, (0, 0))
     screen.blit(character, (character_x_pos, character_y_pos))
 
