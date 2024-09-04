7e35d710b78413400fd1c6e31ca31ac506993e5a
bunnies <m029s20104@gw1.kr>
2024-09-04 13:35:23 +0900

    khj

--- khj.py
+++ khj.py
@@ -5,12 +5,22 @@ pygame.init() #초기화
 #화면크기
 screen_width = 480 # 가로 크기
 screen_height = 640 
-pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width,screen_height))
 
 
 pygame.display.set_caption("bunnies")
 
-사진 = pygame.image.load("C:\\Users\\ncc7\\Desktop\\khj\\ncc\\hwijun\\asset\\gg.png")
+h = pygame.image.load("C:\\Users\\ncc7\\Desktop\\khj\\ncc\\hwijun\\asset\\gg.png")
+
+cha = pygame.image.load("C:\\Users\\ncc7\\Desktop\\khj\\ncc\\hwijun\\asset\\cha.png")
+cha_size = cha.get_rect().size
+cha_width = cha_size[0]
+cha_height = cha_size[1]
+cha_x_pos = screen_width / 2
+cha_y_pos = screen_height
+
+
+
 
 
 running = True
@@ -18,4 +28,12 @@ while running:
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                   running = False
+
+    screen.blit(h, (0, 0))       
+    screen.blit(cha_x_pos,cha_y_pos )
+    pygame.display.update() 
+
+
+
+
 pygame.quit()
