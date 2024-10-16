2e665d2c6c16bde845fea305c25b805fb9f7c190
bunnies <m029s20104@gw1.kr>
2024-10-16 13:35:12 +0900

    rlf

--- gsh/gsh.py
+++ gsh/gsh.py
@@ -14,6 +14,10 @@ pygame.init() #초기화
 폰트글꼴 = "C:/Windows/Fonts/malgun.ttf"
 폰트크기 = 20
 폰트 = pygame.font.Font(폰트글꼴,폰트크기)
+for event in pygame.event.get():
+    폰트출력 = "pygame.KEYDOWN"
+폰트색 = 폰트.render(폰트출력, True, (255, 255, 255))
+폰트위치 = 폰트색.get_rect(center=(가로 // 2, 세로 // 2))
 
 게임 = pygame.display.set_caption("오딘마지막전사")
 
@@ -22,7 +26,17 @@ while 오딘:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             오딘 = False
-
     프레임.tick (60)
+
+    화면.blit(폰트색, 폰트위치)
+
+
+
+
+
+    pygame.display.flip()
+    
+
+
 
 #아아ㅏㅇ아ㅏ아아
