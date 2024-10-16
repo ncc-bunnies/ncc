e4fd5ccc6b72432a08ccf9a1c3b87b9a7d17c294
rlftkdgus11 <rlftkdgus11@gmail.com>
2024-10-16 19:24:15 +0900

    rlftkdgus

--- gsh/gsh.py
+++ gsh/gsh.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec6adaf2cf332d42db0994eaf198e7ae94621f7a974a03951771c868cbdcdf7c
-size 552
+oid sha256:7db5df844833d05a6e5d867f24af82e1ae4cf94ae33d58c7629b4dd4fd84ebc1
+size 361
