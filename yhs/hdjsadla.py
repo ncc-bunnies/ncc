6323af7929f9df12ebc9c350905fd46c9b2d102c
sliwon0897 <wangteulimeongji@gmali.com>
2024-10-23 12:53:30 +0900

    yhs

--- yhs/hdjsadla.py
+++ yhs/hdjsadla.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65f5eec1ab069320522342c0a3bde338da9409150cfb2a99d85c7da3aa60de7d
-size 3194
+oid sha256:0afccca4d294825342d96b4ccea74cdea6b032044f889ee263203ee7a8906785
+size 3183
