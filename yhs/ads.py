0fe504be2f4f0c661b83b10526a6b610b4141ca1
sliwon0897 <wangteulimeongji@gmail.com>
2024-12-02 13:33:41 +0900

    yhs

--- yhs/ads.py
+++ yhs/ads.py
@@ -8,13 +8,13 @@ app=Ursina(
 class Player(FirstPersonController):
     def __init__(self):
         super().__init__(
-            position=(0,11,0),
+            position=(0,1,0),
             scale=1,
             model='cube',
             collider='box',
             texture='white_cube',
             gravity=1.7,
-            jump_height=13,
+            jump_height=6,
             visible_self=True
         )
         
@@ -30,22 +30,35 @@ player=Player()
 plane=Entity(
     model='Plane',
     color=color.green,
-    scale=(50000,1,50000),
+    scale=(50000,-15,50000),
     position=0,
     collider='box',
 )
 
 wall=Entity(
     model='cube',
-    color=color.black,
+    color=color.red,
     position=10,
     scale=(5,25,5),
 )
 
 aa=Button(
     text='32',
-    origin=(1,0)
+    color=color.black50,
+    origin=(-1.25,1.45),
+    scale=(0.5,0.25)
 )
+
+class Voxel(Button):
+    def __init__(self, position=(0,0,0)):
+        super().__init__(parent=scene,
+            position=position,
+            model='cube',
+            origin_y=.5,
+            texture='white_cube',
+            color=color.hsv(0, 0, random.uniform(.9, 1.0)),
+            highlight_color=color.lime,
+        )
 
 def input(key):
     if key=='escape':
@@ -58,5 +71,7 @@ def input(key):
             not window.fullscreen
         else:
             window.fullscreen
+
+voxel=Voxel(position=(1,3,0))
 
 app.run()
